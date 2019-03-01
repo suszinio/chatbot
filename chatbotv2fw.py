b42a06b658daa33d0a0c8fa9029f04dd5f412973
Administrator <33376950+suszinio@users.noreply.github.com>
2019-03-01 16:47:37 +0100

    add some new contextual talk

--- chatbotv2fw.py
+++ chatbotv2fw.py
@@ -58,7 +58,7 @@ def classify(sentence):
     # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse = True)
     return_list = []
-    for r in results:
+    for r in results: 
         return_list.append((classes[r[0]], r[1]))
     #return tuple of intent and probability
     return return_list
